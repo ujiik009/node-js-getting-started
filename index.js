a2f7162b0768d7817c4f100ab6425cb85c95983e
LDC.ICE <ikool009@hotmail.com>
2021-05-29 07:44:02 +0700

    update

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const PORT = process.env.PORT || 80
 app.use(cors())
 var bodyParser = require('body-parser');
 
-// Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+// parse application/json
+app.use(bodyParser.json())
 
 
 app
@@ -23,9 +25,9 @@ app.get('/', (req, res) => {
   })
 })
 
-app.post("/webhook",urlencodedParser ,(req,res)=>{
-  console.log(JSON.stringify(res.body));
-    res.status(200).json(req.body)
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.body)
 })
 
 
