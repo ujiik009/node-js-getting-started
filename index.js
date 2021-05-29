89e7c85f86e42580aebc884c6e08bb92511cb546
LDC.ICE <ikool009@hotmail.com>
2021-05-29 10:21:08 +0700

    - install binance SDK

--- index.js
+++ index.js
@@ -9,7 +9,32 @@ const qs = require("querystring");
 app.use(cors())
 var bodyParser = require('body-parser');
 
+// binance info
+var binance_api_key = "Cvd9DVxoSiHmcWKz7KrEVFXJ6Ylzy3mG4gqLz4B9TOlOBxu755NMdeWED1yjsPac"
+var binance_secret_key = "6tny6Jsdj7I0YiYqpKsb5UZI9tRKI2HDly7GyNzry2FQu2hfMp9jvGUYrNK7iwk4"
+const Binance = require('node-binance-api');
+const binance = new Binance().options({
+  APIKEY: binance_api_key,
+  APISECRET: binance_secret_key,
+  useServerTime: true,
+  recvWindow: 60000, // Set a higher recvWindow to increase response timeout
+  verbose: true, // Add extra output when subscribing to WebSockets, etc
+  log: log => {
+    console.log(log); // You can create your own logger here, or disable console output
+  }
+});
 
+
+function check_coin_balance(coin) {
+  return new Promise(async (r, j) => {
+    await binance.useServerTime();
+    binance.balance((error, balances) => {
+      if (error) return console.error(error);
+      r(balances[coin].available)
+    });
+  })
+
+}
 
 // parse application/json
 app.use(bodyParser.json())
@@ -34,7 +59,7 @@ app.post("/webhook", async (req, res) => {
   const message_notify = await axios.post(
     "https://notify-api.line.me/api/notify",
     qs.stringify({
-      message: `\nCOIN : ${req.body.COIN}\nACTION : ${req.body.ACTION} `,
+      message: `\nCOIN : ${req.body.COIN1}${req.body.COIN2}\nACTION : ${req.body.ACTION} `,
     }),
     {
       headers: {
@@ -45,10 +70,17 @@ app.post("/webhook", async (req, res) => {
   );
 
   if (message_notify.data.status != 200) {
-    console.log(new Date(),"Can not Send to LINE notify",req.body);
-  } 
+    console.log(new Date(), "Can not Send to LINE notify", req.body);
+  }
 
   res.status(200).json(req.body)
+})
+
+app.get("/my/balance", async (req, res) => {
+  console.log("DOGE",await check_coin_balance("DOGE")); 
+  console.log("USDT",await check_coin_balance("USDT")); 
+
+  res.send("OK") 
 })
 
 
